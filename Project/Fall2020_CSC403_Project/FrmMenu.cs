690a6484bc905570e8defa3e81d87642f949f750
thomasn2712 <112751375+thomasn2712@users.noreply.github.com>
2022-10-26 07:35:49 -0500

    Created main menu and fullscreen button

---
+++ Project/Fall2020_CSC403_Project/FrmMenu.cs
@@ -0,0 +1,45 @@
+﻿using Fall2020_CSC403_Project.code;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fall2020_CSC403_Project
+{
+    public partial class FrmMenu : Form
+    {
+        public FrmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FrmLevel level = new FrmLevel();
+            level.Show();
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.TopMost = true;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+        }
+    }
+}
